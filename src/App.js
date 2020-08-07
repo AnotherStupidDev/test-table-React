3f9e6c2459d9a3d207b1e709ec7a78a15b6aab25
AnotherStupidDev <fx014@yandex.ru>
2020-08-07 19:54:49 +0500

    Added custom pagination, removed Lodash orderBy function, added filtration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import Loader from "./components/Loader/Loader"
 import { Table } from "./components/Table/Table"
-import _ from "lodash"
+import { TableSearch } from "./components/TableSearch/TableSearch"
 import DetailRowInfo from "./components/DetailRowInfo/DetailRowInfo"
 import DataVolumeSelector from "./components/DataVolumeSelector/DataVolumeSelector"
+import { Pagination } from "./components/Pagination/Pagination"
 
 const App = () => {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(false)
-  const [sortType, setSortType] = useState("asc")
+  const [sortType, setSortType] = useState("desc")
   const [sortField, setSortField] = useState("")
   const [showRowInfo, setShowRowInfo] = useState(null)
   const [isDataVolumeSelected, setIsDataVolumeSelected] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [rowsPerPage] = useState(28)
+  const [searchField, setSearchField] = useState("")
 
   const onSortHandler = (sortFieldName) => {
     const dataClone = [...data]
-    const sortDirection = sortType === "asc" ? "desc" : "asc"
-    const orderedData = _.orderBy(dataClone, sortFieldName, sortDirection)
+    let sortDirection = sortType === "asc" ? 1 : -1
+
+    const orderedData = dataClone.sort((a, b) => {
+      if (a[sortFieldName] === b[sortFieldName]) return 0
+      return a[sortFieldName] > b[sortFieldName]
+        ? sortDirection
+        : sortDirection * -1
+    })
+
+    sortDirection = sortDirection * -1
+    sortDirection === 1 ? (sortDirection = "asc") : (sortDirection = "desc")
 
     setData(orderedData)
     setSortType(sortDirection)
@@ -33,6 +46,30 @@ const App = () => {
     fetchData(url)
   }
 
+  const searchHandler = (search) => {
+    setSearchField(search)
+    setCurrentPage(1)
+  }
+
+  const resetHandler = () => {
+    setSearchField("")
+  }
+
+  const getFilteredData = () => {
+    if (!searchField) {
+      return data
+    }
+
+    return data.filter((elem) => {
+      return (
+        elem["firstName"].toLowerCase().includes(searchField.toLowerCase()) ||
+        elem["lastName"].toLowerCase().includes(searchField.toLowerCase()) ||
+        elem["email"].toLowerCase().includes(searchField.toLowerCase()) ||
+        elem["phone"].toLowerCase().includes(searchField.toLowerCase())
+      )
+    })
+  }
+
   async function fetchData(url) {
     const response = await fetch(url)
     const data = await response.json()
@@ -40,9 +77,15 @@ const App = () => {
     setIsLoading(false)
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+  // Get Current Rows
+  const indexOfLastRow = currentPage * rowsPerPage
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage
+  const currentData = getFilteredData().slice(indexOfFirstRow, indexOfLastRow)
+  const paginateHandler = (pageNumber) => setCurrentPage(pageNumber)
+
+  // useEffect(() => {
+  //   // fetchData()
+  // }, [])
 
   if (!isDataVolumeSelected) {
     return (
@@ -51,21 +94,31 @@ const App = () => {
       </div>
     )
   }
-
+  console.log("AAAAA")
   return (
     <div className="container">
       {isLoading ? (
         <Loader />
       ) : (
-        <Table
-          data={data}
-          onSort={onSortHandler}
-          sort={sortType}
-          sortField={sortField}
-          onRowSelect={onRowSelectHandler}
-        />
+        <React.Fragment>
+          <TableSearch onSearch={searchHandler} onReset={resetHandler} />
+          <Table
+            data={currentData}
+            onSort={onSortHandler}
+            sort={sortType}
+            sortField={sortField}
+            onRowSelect={onRowSelectHandler}
+          />
+        </React.Fragment>
       )}
       {showRowInfo ? <DetailRowInfo person={showRowInfo} /> : null}
+      {rowsPerPage > getFilteredData().length ? null : (
+        <Pagination
+          rowsPerPage={rowsPerPage}
+          totalRows={data.length}
+          paginate={paginateHandler}
+        />
+      )}
     </div>
   )
 }
